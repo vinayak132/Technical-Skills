ab933f8d0438001a9919760f245356d2ada2d305
Vinayak Kaushik <vinayakkaushik02@gmail.com>
2026-02-04 11:05:28 +0530

    c

--- Technical Skills/Q3.java
+++ Technical Skills/Q3.java
@@ -4,7 +4,7 @@
 public class Q3{
     public static void main(String []args){ 
         int a[]={2,3,5,6,4};
-        int max;
+        int max=a[i];
         int n=a.lenght;
         int time=0
         for(int i=0;i<n;i++){
